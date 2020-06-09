631f4cc1a73ed386d9c2426f459858d79f3c6de8
cficht <chris.ficht@gmail.com>
2020-06-08 17:30:05 -0700

    netlify test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Projects from '../Projects/Projects';
-import styles from './App.css';
+import './App.css';
 
 export default function App() {
   return (
     <>
       {/* <div className={styles.three_box}> */}
-        <Projects />
+      YO
+      <Projects />
       {/* </div> */}
     </>
   );
